1e441850c046ed7dafe259983ff01be48ba4f89d
wilamc <wilamchen99@gmail.com>
2023-04-29 09:47:38 -0700

    Testing with event listenersfor buttons

--- calculator.js
+++ calculator.js
@@ -1,15 +1,62 @@
+const numberBtns = document.querySelectorAll('.number');
+const operatorBtns = document.querySelectorAll('.operator');
+const bottomScreen = document.querySelector('.bottom-screen');
+const topScreen = document.querySelector('.top-screen');
+let currentValue = '';
+let previousValue = '';
+
+numberBtns.forEach(button => {
+  button.addEventListener('click', (e) => {
+    currentValue += e.target.textContent;
+    bottomScreen.textContent = currentValue;
+  })
+  
+})
+
+operatorBtns.forEach(button => {
+  button.addEventListener('click', (e) => {
+    previousValue = currentValue
+    currentValue = topScreen.textContent
+    topScreen.textContent = previousValue
+    topScreen.textContent += e.target.textContent
+  })
+})
+
+function clear() {
+  bottomScreen.textContent = '';
+  topScreen.textContent = '';
+}
+
 function add(a, b) {
-  return a + b;
+  bottomScreen.textContent += (a + b);
 }
 
 function subtract(a, b) {
-  return a - b;
+  bottomScreen.textContent += (a - b);
 }
 
 function multiply(a, b) {
-  return a * b;
+  bottomScreen.textContent += (a * b);
 }
 
 function divide(a, b) {
-  return a / b;
+  bottomScreen.textContent += (a / b);
+}
+
+// Performs the calculation
+function operate(operator, a, b) {
+  switch (operator) {
+    case '+':
+      add(a, b);
+      break;
+    case '-':
+      subtract(a, b);
+      break;
+    case '×':
+      multiply(a, b);
+      break;
+    case '÷':
+    divide(a, b);
+    break;
+  }
 }
