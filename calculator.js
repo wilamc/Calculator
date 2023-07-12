0ff7961f1a5d72dd4ba487abf90100ab2b2797cf
wilamc <wilamchen99@gmail.com>
2023-07-11 23:21:53 -0700

    DOMs, event listeners for num and operator button

--- calculator.js
+++ calculator.js
@@ -1,62 +1,57 @@
-const numberBtns = document.querySelectorAll('.number');
+const numberBtns = document.querySelectorAll('.number'); //setting DOMs
 const operatorBtns = document.querySelectorAll('.operator');
+const equalBtn = document.querySelector('.equal');
+const clearBtn = document.querySelector('#clear');
+const backspaceBtn = document.querySelector('#backspace');
 const bottomScreen = document.querySelector('.bottom-screen');
 const topScreen = document.querySelector('.top-screen');
-let currentValue = '';
+let currentValue = ''; //creating values that can be changeable and replaced
 let previousValue = '';
+let currentOperator = null;
+let shouldResetScreen = false;
 
-numberBtns.forEach(button => {
-  button.addEventListener('click', (e) => {
-    currentValue += e.target.textContent;
+numberBtns.forEach(button => { //create event listeners for each number button
+  button.addEventListener('click', () => {
+    if (shouldResetScreen) {
+      resetScreen();
+    }
+    currentValue += button.textContent; //capture the clicked number and display it
     bottomScreen.textContent = currentValue;
   })
-  
 })
 
-operatorBtns.forEach(button => {
-  button.addEventListener('click', (e) => {
-    previousValue = currentValue
-    currentValue = topScreen.textContent
-    topScreen.textContent = previousValue
-    topScreen.textContent += e.target.textContent
+operatorBtns.forEach(button => { //create event listener for each operator button
+  button.addEventListener('click', () => {
+    if (currentValue !== '') {
+      if(currentOperator !== null && previousValue !== '') {
+        performCalculation(); //call function if there is an operator and value
+      } //if there is a current value, set an operator and display it
+    currentOperator = button.textContent;
+    previousValue = currentValue;
+    currentValue = '';
+    topScreen.textContent = previousValue + ' ' + currentOperator;
+    shouldResetScreen = false;
+    } else if (previousValue !== '' && currentOperator !== null) {
+      currentOperator = button.textContent;
+      topScreen.textContent = previousValue + ' ' + currentOperator;
+    }//if we want to change the current operator, else if with criteria
   })
 })
 
-function clear() {
-  bottomScreen.textContent = '';
-  topScreen.textContent = '';
-}
-
-function add(a, b) {
-  bottomScreen.textContent += (a + b);
-}
-
-function subtract(a, b) {
-  bottomScreen.textContent += (a - b);
-}
-
-function multiply(a, b) {
-  bottomScreen.textContent += (a * b);
-}
-
-function divide(a, b) {
-  bottomScreen.textContent += (a / b);
-}
-
-// Performs the calculation
-function operate(operator, a, b) {
-  switch (operator) {
+function calculate(operator, a, b) { //calculates taking in operator and 2 numbers
+  switch(operator) {
     case '+':
-      add(a, b);
-      break;
+      return a + b;
     case '-':
-      subtract(a, b);
-      break;
+      return a - b;
     case '×':
-      multiply(a, b);
-      break;
+      return a * b;
     case '÷':
-    divide(a, b);
-    break;
+      if (b === 0) {
+        return "Error: Division by Zero";
+      }
+      return a / b;
+    default:
+      return "Error: Invalid operator";
   }
 }
